e5e188086f6e8c2c75cfce8b99e93b2337fb6bd9
Lmstrgb <lmstrmax@gmail.com>
2023-02-26 22:14:32 +0500

    Задача 6

---
+++ Sem1_HomeWork_1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+void MaxFunc(int a, int b, int c)
+{
+
+  int max=a;
+  if (max < b)
+  {
+    max = b;   
+  }
+  if (max < c)
+  {
+    max = c;
+  }
+
+  Console.WriteLine("Max = " + max);
+
+}
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+MaxFunc(num1, num2, num3);
---
+++ Sem1_HomeWork_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+string DivBy2(int a)
+{
+  string result = "нечетное";
+  if (a % 2 == 0)
+  {
+    result = "четное";
+  }
+  return result;
+}
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string result = DivBy2(num1);
+Console.WriteLine("Число " + num1 + " - " + result);
+
