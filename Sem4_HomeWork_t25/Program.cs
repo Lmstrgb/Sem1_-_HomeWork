aac35f12b7dcbda87720e0b95c648dca3fda765f
Lmstrgb <lmstrmax@gmail.com>
2023-03-08 21:20:06 +0500

    Задачи 25, 27, 29

---
+++ Sem4_HomeWork_t25/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//Функция приветствия и проверки вводимого на признак числа
+int InputInt(string msg)
+{
+  Console.Write(msg);
+  string Str = Console.ReadLine()!;
+  int res = 0;
+  if (!int.TryParse(Str, out int num1))
+  {
+    Console.WriteLine("Просили же ввести число, попробуйте еще раз.");
+    Console.Write($"{Environment.NewLine}Press any key to exit...");
+    Console.ReadKey(true);
+    Environment.Exit(0); //чтобы исключение не лезло
+  }
+  else { res = Convert.ToInt32(Str); }
+
+  return res;
+}
+
+
+bool ValidateExp(int exp)
+{
+  if (exp < 0)
+  {
+    Console.WriteLine("Степень отрицательная");
+    return false;
+  }
+  else return true;
+}
+
+
+int PlusExp(int baseOfDigit, int exponent)
+{
+  int digitInDegree = 1;
+  for (int i = 0; i < exponent; i++)
+  {
+    digitInDegree *= baseOfDigit;
+  }
+  return digitInDegree;
+}
+
+//Функция возведения в отрицательную степень
+double NegExp(double baseOfDigit, double exponent)
+{
+  if (baseOfDigit == 0)
+  {
+    Console.WriteLine("Ноль не может быть возведен в отрицательную степень");
+    Console.Write($"{Environment.NewLine}Press any key to exit...");
+    Console.ReadKey(true);
+    Environment.Exit(0); //чтобы исключение не лезло
+    return 0;
+  }
+  else
+  {
+    double digitInDegree = 1.0;
+    for (int i = 0; i < Math.Abs(exponent); i++)
+    {
+      digitInDegree *= 1.0 / baseOfDigit;
+    }
+    return digitInDegree;
+  }
+}
+
+
+int digit = InputInt("Введите число: ");
+int exponent = InputInt("Введите стпень: ");
+double digitDouble = Convert.ToDouble(digit);
+double exponentDouble = Convert.ToDouble(exponent);
+
+if (ValidateExp(exponent))
+{
+  Console.WriteLine($"Число {digit} в степени {exponent} равно {PlusExp(digit, exponent)}");
+}
+else
+{
+  Console.WriteLine($"Число {digit} в степени {exponent} равно {Math.Round(NegExp(digitDouble, exponentDouble), 5)}");
+}
+
---
+++ Sem4_HomeWork_t27_t29/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//Функция приветствия и проверки вводимого на признак числа
+int InputInt(string msg)
+{
+  Console.Write(msg);
+  string Str = Console.ReadLine()!;
+  int res = 0;
+  if (!int.TryParse(Str, out int num1))
+  {
+    Console.WriteLine("Просили же ввести число, попробуйте еще раз.");
+    Console.Write($"{Environment.NewLine}Press any key to exit...");
+    Console.ReadKey(true);
+    Environment.Exit(0); //чтобы исключение не лезло
+  }
+  else { res = Convert.ToInt32(Str); }
+
+  return res;
+}
+
+//Функция число -> массив, состоящий из цифр числа
+int[] FuncDigitToMass(int numf)
+{
+  int[] mass1 = new int[numf];
+  for (int i = 0; i < mass1.Length; i++)
+  {
+    mass1[mass1.Length - i - 1] = numf % 10;
+    numf = numf / 10;
+  }
+  return mass1;
+}
+
+//Функция определения суммы элементов в массиве
+int FuncMassSumm(int[] mass2)
+{
+  int result = 0;
+  for (int i = 0; i < mass2.Length; i++)
+  {
+    result += mass2[i];
+  }
+  return result;
+}
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//Функция наполнения массива случайными часлами
+void FuncFillMassRandom(int[] mass3)
+{
+  for (int i=0; i<mass3.Length; i++)
+  {
+    mass3[i] = new Random().Next(0,101);
+  }
+}
+
+
+//Задача t25
+// int digit = InputInt("Введите число: ");
+// int[] mass = new int[digit];
+// mass = FuncDigitToMass(digit);
+// Console.WriteLine($"Сумма цифр числа {digit} = {FuncMassSumm(mass)}");
+
+//Задача t29
+int number = InputInt("Введите размерность массива: ");
+int [] mass = new int[number];
+FuncFillMassRandom(mass);
+Console.WriteLine($"Сумма цифр числа {number} = [{String.Join(" ", mass)}]");
